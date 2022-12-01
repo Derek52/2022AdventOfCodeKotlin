b144287656776eef868c71ecf27ed7f109503d43
Derek Briggs <contact@derekbriggs.xyz>
2022-12-01 15:10:02 -0500

    Solve both parts of Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,66 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val input = readInput("day1_part1")
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    var highestCalorieCount = 0
+    var currentCalorieCount = 0
+
+    for (line in input) {
+        if (line == "") {
+            if (currentCalorieCount > highestCalorieCount) {
+                println("$currentCalorieCount is higher than highest: $highestCalorieCount")
+                highestCalorieCount = currentCalorieCount
+            }
+            currentCalorieCount = 0
+        } else {
+            currentCalorieCount += line.toInt()
+        }
+    }
+    return highestCalorieCount
+}
+
+fun part2(input: List<String>): Int {
+    var currentCalorieCount = 0
+    val highestTotalList = MutableList<Int>(3) { 0 }
+
+    for (line in input) {
+        if (line == "") {
+            if (currentCalorieCount > highestTotalList[0]) {
+                highestTotalList[0] = currentCalorieCount
+                bubbleSort(highestTotalList)
+            }
+            currentCalorieCount = 0
+        } else {
+            currentCalorieCount += line.toInt()
+        }
+    }
+
+    return sumList(highestTotalList)
+}
+
+fun bubbleSort(list: MutableList<Int>) {
+    var highestValue = 0
+    var indexOfHighest = 0
+    for (i in list.size - 1 downTo 2) {
+        for (j in 0 until i) {
+            val a = list[j]
+            val b = list[j+1]
+            if (a > b) {
+                list[j] = b
+                list[j+1] = a
+            }
+        }
+    }
+}
+
+fun sumList(list: List<Int>) : Int {
+    var currentSum = 0
+    list.forEach {
+        currentSum += it
+    }
+    return currentSum
 }
