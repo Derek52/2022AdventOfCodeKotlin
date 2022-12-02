6c33632a48122f34047ae255c8f95727e4ef82d8
Derek Briggs <contact@derekbriggs.xyz>
2022-12-01 21:14:42 -0500

    Setup files for the next couple of days.

---
+++ src/day3/Day03.kt
@@ -0,0 +1,18 @@
+package day3
+
+import readInput
+
+fun main() {
+    val input = readInput("day3/day3")
+
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>) {
+
+}
+
+fun part2(input: List<String>) {
+
+}
