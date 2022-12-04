d195e0f4c740394e0369e6b95378f296fff0cebb
Derek Briggs <contact@derekbriggs.xyz>
2022-12-04 02:05:38 -0500

    Solve Day 4.

--- src/main/day4/Day04.kt
+++ src/main/day4/Day04.kt
@@ -16,12 +16,62 @@ fun main() {
     }
 }
 
-fun part1(input: List<String>) {
+data class Range(val x:Int, val y:Int)
 
+fun rangeInRange(left: Range, right: Range) : Boolean {
+    return (left.x <= right.x && left.y >= right.y) || (right.x <= left.x && right.y >= left.y)
+}
+fun rangeOverlapsRange(left: Range, right: Range) : Boolean{
+    return (right.x >= left.x && right.x <= left.y) || (right.y >= left.x && right.y <= left.y) ||
+            (left.x >= right.x && left.x <= right.y) || (left.y >= right.x && left.y <= right.y)
 }
 
-fun part2(input: List<String>) {
+fun part1(input: List<String>) : Int{
+    var overlapCount = 0
+    for (line in input) {
+        val assignments = line.split(',')
+        val elf1 = Assignment(assignments[0])
+        val elf2 = Assignment(assignments[1])
 
+
+        var overlap = if (elf1.range.x <= elf2.range.x) {
+            rangeInRange(elf1.range, elf2.range)
+        } else {
+            rangeInRange(elf2.range, elf1.range)
+        }
+
+        if (overlap) {
+            overlapCount++
+        }
+    }
+    return overlapCount
+}
+
+fun part2(input: List<String>) : Int {
+    var overlapCount = 0
+    for (line in input) {
+        val assignments = line.split(',')
+        val elf1 = Assignment(assignments[0])
+        val elf2 = Assignment(assignments[1])
+
+
+        if (rangeOverlapsRange(elf1.range, elf2.range)) {
+            overlapCount++
+        }
+    }
+    return overlapCount
+}
+
+data class Assignment(val assignment: String) {
+    var a: Int = 0
+    var b: Int = 0
+    var range = Range(0, 0)
+    init {
+        val boundaries = assignment.split("-")
+        a = boundaries[0].toInt()
+        b = boundaries[1].toInt()
+        range = Range(a, b)
+    }
 }
 
 fun testAlg(firstHalf : Boolean) {
